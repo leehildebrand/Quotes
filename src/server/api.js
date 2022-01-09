cadc1a5e34552d4eb617fb27720f9600b762617e
Lee HIldebrand <apple@leehildebrand.name>
2022-01-08 22:09:44 -0500

    start connection

--- src/server/api.js
+++ src/server/api.js
@@ -2,25 +2,32 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
-const path = require('path');
+
+const jsforce = require('jsforce');
+require('dotenv').config();
+const { SF_USERNAME, SF_PASSWORD, SF_TOKEN, SF_LOGIN_URL } = process.env;
+if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
+    console.error(
+        'Cannot start app: missing mandatory configuration. Check your .env file.'
+    );
+    process.exit(-1);
+}
+const conn = new jsforce.Connection({
+    loginUrl: SF_LOGIN_URL
+});
+conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, err => {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+});
 
 const app = express();
 app.use(helmet());
 app.use(compression());
 
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3002;
-const DIST_DIR = './dist';
-
-app.use(express.static(DIST_DIR));
-
-app.use(/^(?!\/api).+/, (req, res) => {
-    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
-});
-
-app.get('/api/v1/endpoint', (req, res) => {
-    res.json({ success: true });
-});
+const HOST = process.env.API_HOST || 'localhost';
+const PORT = process.env.API_PORT || 3002;
 
 app.get('/api/quotes', (req, res) => {
     const soql = `SELECT Text__c FROM Quote__c`;
