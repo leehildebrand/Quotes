f53b01cc7ba3b027b7c94441552ffdaed5475039
Lee HIldebrand <apple@leehildebrand.name>
2022-01-09 22:59:42 -0500

    Order Function Fixes #10

--- src/server/api.js
+++ src/server/api.js
@@ -52,7 +52,7 @@ app.get('/api/v1/endpoint', (req, res) => {
 });
 
 app.get('/api/quotes', (req, res) => {
-    const soql = `SELECT Text__c FROM Quote__c ORDER BY Name`;
+    const soql = `SELECT Text__c FROM Quote__c ORDER BY Order__c`;
     conn.query(soql, (err, result) => {
         if (err) {
             res.sendStatus(500);
