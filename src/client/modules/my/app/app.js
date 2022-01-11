8bc6760c6d02f6372ae52aa4346298f785ee3ac2
Lee HIldebrand <apple@leehildebrand.name>
2022-01-11 16:38:50 -0500

    load in lwc-oss-oauth code

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { getLoggedInUser } from 'data/authService';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    @track loggedUser = undefined;
+    @track state;
+
+    connectedCallback() {
+        getLoggedInUser().then((response) => {
+            if (response.user_id === undefined) {
+                this.loggedUser = undefined;
+                this.state = 'login';
+            } else {
+                this.loggedUser = response;
+                this.state = 'list';
+            }
+        });
+    }
+
+    get isLoginView() {
+        return this.state === 'login';
+    }
+
+    get isSessionListView() {
+        return this.state === 'list';
+    }
+}
+``
